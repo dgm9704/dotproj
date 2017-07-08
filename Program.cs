f99b5d08f329a4a663dd631f49c2daacee654ec5
John Nordberg <john.nordberg@gmail.com>
2017-07-09 01:57:22 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace dotproj
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => CreateDotFile(ParseFolder(args[0]), args[1]);
+
+        static Dictionary<string, HashSet<string>> ParseFolder(string path)
+        {
+            return Directory.EnumerateFiles(path, "*.*proj", SearchOption.AllDirectories).
+                ToDictionary(
+                    project => project.GetProjectName(),
+                    project => new HashSet<string>(ParseProjectReferences(project)));
+        }
+
+        static IEnumerable<string> ParseProjectReferences(string path)
+        {
+            return XDocument.Load(path).
+                    Elements("ProjectReference").
+                    Select(r => r.Attribute("Include").Value.GetProjectName());
+        }
+
+        static void CreateDotFile(Dictionary<string, HashSet<string>> entries, string path)
+        {
+            File.WriteAllLines(path, entries.Keys);
+        }
+    }
+
+    internal static class StringExtensions
+    {
+        public static string GetProjectName(this string Value)
+            => Path.GetFileNameWithoutExtension(Value);
+    }
+}
