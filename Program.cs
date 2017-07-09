133017ac24ba1e3e1b2ee6d826059e926b7786fb
John Nordberg <john.nordberg@gmail.com>
2017-07-09 20:55:32 +0300

    working with .sln

--- Program.cs
+++ Program.cs
@@ -9,7 +9,18 @@ namespace dotproj
     class Program
     {
         static void Main(string[] args)
-            => CreateDotFile(ParseFolder(args[0]), args[1]);
+        {
+            var isSolution = Path.HasExtension(args[0]);
+            var projects = isSolution
+                ? ParseSolution(args[0])
+                : ParseFolder(args[0]);
+
+            var containerName = isSolution
+                ? Path.GetFileNameWithoutExtension(args[0])
+                : Path.GetDirectoryName(args[0]);
+
+            CreateDotFile(args[1], projects, containerName);
+        }
 
         static Dictionary<string, HashSet<string>> ParseFolder(string path)
         {
@@ -19,31 +30,80 @@ namespace dotproj
                     project => new HashSet<string>(ParseProjectReferences(project)));
         }
 
-        static IEnumerable<string> ParseProjectReferences(string path)
+        static Dictionary<string, HashSet<string>> ParseSolution(string path)
         {
-            return XDocument.Load(path).Root.
-                    Descendants("ProjectReference").
-                    Select(r => r.
-                        Attribute("Include").Value);
+            return GetProjectsInSolution(path).
+                ToDictionary(
+                    project => project,
+                    project => new HashSet<string>(ParseProjectReferences(project)));
         }
 
-        static void CreateDotFile(Dictionary<string, HashSet<string>> entries, string path)
+        static IEnumerable<string> GetProjectsInSolution(string solutionFile)
+        {
+            return File.ReadAllLines(solutionFile).
+                Where(l => l.StartsWith("Project")).
+                Select(l => l.
+                    Split('=').Last().
+                    Split(',').Skip(1).First().
+                    Trim().Trim('"')).
+                Select(p => Path.GetFullPath(Path.Combine(Path.GetDirectoryName(solutionFile), p)).Replace("\\", "/"));
+        }
+
+        static IEnumerable<string> ParseProjectReferences(string path)
+        {
+            // return XDocument.Load(path).Root.
+            //         Descendants("ProjectReference").
+            //         Select(r => r.
+            //             Attribute("Include").Value);
+            var doc = XDocument.Load(path);
+            var root = doc.Root;
+            var ns = root.GetDefaultNamespace();
+            return root.
+                Descendants(ns + "ProjectReference").
+                Select(r => r.Attribute("Include").Value);
+        }
+
+        static void CreateDotFile(string path, Dictionary<string, HashSet<string>> entries, string containerName)
         {
             File.WriteAllText(path, "digraph g {\n");
+            File.AppendAllText(path, $"subgraph {containerName} {{\n");
             File.AppendAllLines(path, entries.Select(e => CreateDot(e.Key, e.Value)));
-            File.AppendAllText(path, "}");
+            File.AppendAllText(path, "}}");
         }
 
         static string CreateDot(string key, IEnumerable<string> values)
         {
-            return $"{key.GetProjectName().Quote()} -> {{ {values.Select(v => v.GetProjectName().Quote()).Join(", ")} }}";
+            key = key.Split('/', '\\').Last();
+            var idx = key.LastIndexOf('.');
+            var l = key.Substring(idx + 1);
+            var name = key.Remove(idx).Quote();
+            var bgcolor = "white";
+            var fontcolor = "black";
+
+            switch (l)
+            {
+                case "csproj":
+                    bgcolor = "#388A34";
+                    fontcolor = "white";
+                    break;
+                case "vbproj":
+                    bgcolor = "#00539C";
+                    fontcolor = "white";
+                    break;
+                case "fsproj":
+                    bgcolor = "#672878";
+                    fontcolor = "white";
+                    break;
+            }
+
+            return $"{name} [shape=\"box\",color=\"{bgcolor}\", style=\"filled\", fillcolor=\"{bgcolor}\", fontcolor=\"{fontcolor}\"];\n{name} -> {{ {values.Select(v => v.GetProjectName().Quote()).Join(", ")} }}";
         }
     }
 
     internal static class StringExtensions
     {
         public static string GetProjectName(this string Value)
-            => Value.Split('\\', '/').Last();
+            => Value.Substring(0, Value.LastIndexOf('.')).Split('\\', '/').Last();
 
         public static string Join(this IEnumerable<string> values, string separator)
             => string.Join(separator, values);
